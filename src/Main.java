9c0620fd0f0d0f0a4eff25a94d83a22974308bdb
dimitroff2 <dimitroff2@gmail.com>
2023-04-06 15:10:51 -0400

    Inital Commit Learning about statements expressions and whitespace

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        double kilometers = (1000 * 1.609344);
+        int myVariable = 50;
+        //expressions are everything except usually the semi colons the paratheses and the keywords
+
+        myVariable++;
+        myVariable--;
+        //the semicolon makes it a complete statment without semicolon its just an expression
+
+        System.out.println("This is " +
+                "another " +
+                "still more.");
+        //statements dont have to be all on one line
+
+        int anotherVariable = 50; myVariable--; System.out.println("myVariable =" + myVariable);
+        //multiple statements can be put on one line because it found the semi-colon between statements to separate them.
+
+        /* Whitespace is any extra spacing, horizontally or vertically, placed around Java source code.
+           Its done for humans so they can read Java easier and Java just ignores whitespace
+         */
+
+
+
+    }
+}
